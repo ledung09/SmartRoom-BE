3f034719b96a939b81751f4b6b1f4c0e9db9d5cc
Le Dung <dp09.it@gmail.com>
2024-07-31 23:49:46 +0700

    handling category api

--- src/service/category.py
+++ src/service/category.py
@@ -2,5 +2,5 @@ from database.config import supabase
 
 class categoryService:
     def getCategoryList(self):
-        response = supabase.table("Category").select("*").execute()
-        return response
+        response = supabase.table("Category").select("CategoryName", "Picture").order('CategoryName', desc=True).execute()
+        return response.data
