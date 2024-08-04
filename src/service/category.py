7e39139ba1568f550cc43d8a3462d61423a029d0
ledung09 <121594269+ledung09@users.noreply.github.com>
2024-08-04 17:48:09 +0700

    modify category API

--- src/service/category.py
+++ src/service/category.py
@@ -2,5 +2,5 @@ from database.supabase import supabase
 
 class categoryService:
     def getCategoryList(self):
-        response = supabase.table("category").select("category_name", "picture").order('category_name', desc=False).execute()
+        response = supabase.table("category").select("category_id", "category_name", "picture").order('category_name', desc=False).execute()
         return response.data
